e0c39a207bd1557b141219f796eb6dd1a66470b7
Beau Smith <beau@squareup.com>
2014-02-03 17:59:44 -0800

    Matches 0 or more of the preceding token.

--- tasks/staticinline.js
+++ tasks/staticinline.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
   };
 
   var findReplaceScript = function(templatePath, content, basepath){
-    return content.replace(/<script[^<]src=['"]([^'"]+)['"][^<]inline=['"]true['"][^<]><\/script>/g, function(match, src){
+    return content.replace(/<script[^<]src=['"]([^'"]+)['"][^<]*inline=['"]true['"][^<]*><\/script>/g, function(match, src){
       return baseTAGReplace(templatePath, "script", src, basepath);
     });
   };
